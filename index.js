57d294acd8953df804d641d8d1ab1939b14d2b6b
pratiksj <pratiksharai728@gmail.com>
2022-09-11 16:58:42 +0545

    set final validation to the findByAndUpdate method

--- index.js
+++ index.js
@@ -109,7 +109,11 @@ App.put("/notes/:id", (request, response, next) => {
     important: body.important,
   };
 
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+  Note.findByIdAndUpdate(request.params.id, note, {
+    new: true,
+    runValidators: true,
+    context: "query",
+  })
     .then((updatedNote) => {
       response.json(updatedNote);
     })
