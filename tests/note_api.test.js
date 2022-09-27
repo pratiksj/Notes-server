79583bdfa3b31429c13d4d6f1cc9265a0ce4eea7
pratiksj <pratiksharai728@gmail.com>
2022-09-27 11:59:38 +0545

    refactored the code and grouping the testing

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -18,77 +18,88 @@ const api = supertest(app); // supertest le app lai wrap garyo
 //     important: true,
 //   },
 // ];
+// beforeEach(async () => {
+//   await Note.deleteMany({});
+//   await Note.insertMany(helper.initialNotes); //mongoose library deko ho javascript ma jahapani chaluna mildaina
+// });
 
 beforeEach(async () => {
   await Note.deleteMany({});
 
-  const noteObjects = helper.initialNotes.map((note) => new Note(note));
-  const promiseArray = noteObjects.map((note) => note.save());
+  const noteObjects = helper.initialNotes.map((note) => new Note(note)); //mogoose schema ko new note banako
+  const promiseArray = noteObjects.map((note) => note.save()); //each .save return promise which promiseArray ma promise xa
   await Promise.all(promiseArray);
-  // await Note.deleteMany({});
-  // let noteObject = new Note(helper.initialNotes[0]);
-  // await noteObject.save();
-  // noteObject = new Note(helper.initialNotes[1]);
-  // await noteObject.save();
+}); // yo line ma aayera await prominse.all le sbai promise lai parkhinxa
+//prmise.all chai javascript ma jaha pani chalauna pauxa
+//   //previous code
+//   // await Note.deleteMany({});
+//   // let noteObject = new Note(helper.initialNotes[0]);
+//   // await noteObject.save();
+//   // noteObject = new Note(helper.initialNotes[1]);
+//   // await noteObject.save();
+// });
+
+describe("when there is initially some notes save", () => {
+  test("notes are returned as json", async () => {
+    await api
+      .get("/api/notes")
+      .expect(200)
+      //.expect("Content-Type", "application/json; charset=utf-8");
+      .expect("Content-Type", /application\/json/); // this is regular expression
+    // this is regular expression
+  });
+
+  test("all notes are returned", async () => {
+    const response = await api.get("/api/notes");
+
+    expect(response.body).toHaveLength(helper.initialNotes.length);
+  });
+
+  test("a specific note is within the returned notes", async () => {
+    const response = await api.get("/api/notes");
+
+    const contents = response.body.map((r) => r.content);
+    expect(contents).toContain("Browser can execute only Javascript");
+  });
 });
 
-test("notes are returned as json", async () => {
-  await api
-    .get("/api/notes")
-    .expect(200)
-    //.expect("Content-Type", "application/json; charset=utf-8");
-    .expect("Content-Type", /application\/json/); // this is regular expression
-  // this is regular expression
-});
+describe("addition of a new note", () => {
+  test("a valid note can be added", async () => {
+    const newNote = {
+      content: "async/await simplifies making async calls",
+      important: true,
+    };
 
-test("all notes are returned", async () => {
-  const response = await api.get("/api/notes");
+    await api
+      .post("/api/notes")
+      .send(newNote)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
 
-  expect(response.body).toHaveLength(helper.initialNotes.length);
-});
+    const notesAtEnd = await helper.notesInDb();
+    expect(notesAtEnd).toHaveLength(helper.initialNotes.length + 1);
+    const contents = notesAtEnd.map((n) => n.content);
 
-test("a specific note is within the returned notes", async () => {
-  const response = await api.get("/api/notes");
+    // const contents = notesAtEnd.map(n => n.content)
 
-  const contents = response.body.map((r) => r.content);
-  expect(contents).toContain("Browser can execute only Javascript");
-});
+    // const response = await api.get("/api/notes");
 
-test("a valid note can be added", async () => {
-  const newNote = {
-    content: "async/await simplifies making async calls",
-    important: true,
-  };
+    // const contents = response.body.map((r) => r.content);
 
-  await api
-    .post("/api/notes")
-    .send(newNote)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
+    expect(contents).toContain("async/await simplifies making async calls");
+  });
 
-  const notesAtEnd = await helper.notesInDb();
-  expect(notesAtEnd).toHaveLength(helper.initialNotes.length + 1);
-  const contents = notesAtEnd.map((n) => n.content);
+  test("note without content is not added", async () => {
+    const newNote = {
+      important: true,
+    };
 
-  // const contents = notesAtEnd.map(n => n.content)
+    await api.post("/api/notes").send(newNote).expect(400);
 
-  // const response = await api.get("/api/notes");
+    const response = await api.get("/api/notes");
 
-  // const contents = response.body.map((r) => r.content);
-
-  expect(contents).toContain("async/await simplifies making async calls");
-});
-
-test("note without content is not added", async () => {
-  const newNote = {
-    important: true,
-  };
-
-  await api.post("/api/notes").send(newNote).expect(400);
-
-  const response = await api.get("/api/notes");
-
-  expect(response.body).toHaveLength(helper.initialNotes.length);
+    expect(response.body).toHaveLength(helper.initialNotes.length);
+  });
 });
 
 afterAll(() => {
