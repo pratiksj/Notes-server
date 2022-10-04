79a1158bbc912bd2fa4a2c3cf36a18682d8b7b93
pratiksj <pratiksharai728@gmail.com>
2022-10-04 17:01:54 +0545

    created  a new note

--- tests/user_api.test.js
+++ tests/user_api.test.js
@@ -1,6 +1,9 @@
 const bcrypt = require("bcrypt");
 const User = require("../model/user");
 const helper = require("./test_helper");
+const supertest = require("supertest");
+const app = require("../app");
+const api = supertest(app);
 
 describe("when there is initially one user in db", () => {
   beforeEach(async () => {
@@ -32,5 +35,27 @@ describe("when there is initially one user in db", () => {
 
     const usernames = usersAtEnd.map((u) => u.username);
     expect(usernames).toContain(newUser.username);
+  });
+});
+describe("when there is initially one user in db", () => {
+  test("creation fails with proper statuscode and message if username already taken", async () => {
+    const usersAtStart = await helper.usersInDb();
+
+    const newUser = {
+      username: "root",
+      name: "Superuser",
+      password: "salainen",
+    };
+
+    const result = await api
+      .post("/api/users")
+      .send(newUser)
+      .expect(400)
+      .expect("Content-Type", /application\/json/);
+
+    expect(result.body.error).toContain("username must be unique");
+
+    const usersAtEnd = await helper.usersInDb();
+    expect(usersAtEnd).toEqual(usersAtStart);
   });
 });
